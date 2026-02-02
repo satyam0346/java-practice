3477774daba9998ae1b348b2818d0b2bd377ca04
Satyam Kumar <satyamkumar08642@gmail.com>
2026-02-02 07:02:00 +0530

    Practice of variable

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main (String[] args){
+        String name = "Satyam";
+        int roll = 12;
+        float marks = 95.45f;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println("Name:" + name);
+        System.out.println("Roll No:" + roll);
+        System.out.println("Marks:" + marks);
+        System.out.println("Grade:" + grade);
+        System.out.println("Passes:" + isPassed);
+
+
+    }
+}
