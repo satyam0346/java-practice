5dddda0908db4ce55ec64b553935527e6192b717
Satyam Kumar <satyamkumar08642@gmail.com>
2026-02-14 00:58:04 +0530

    printing number from 1 to 100

---
+++ Practice/Loops/NumberLoop.java
@@ -0,0 +1,9 @@
+public class NumberLoop{
+    public static void main(String [] args){
+        System.out.println("Numbers from 1 to 100 are:");
+
+        for (int i = 1 ; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
