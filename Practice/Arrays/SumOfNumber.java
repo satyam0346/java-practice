12baa851d153b7c2effdef5f1382d396b202ec79
Satyam Kumar <satyamkumar08642@gmail.com>
2026-02-22 01:28:02 +0530

    Sum of element of Array

---
+++ Practice/Arrays/SumOfNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class SumOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int n = sc.nextInt();
+        int numbersArray [] = new int [n];
+
+
+        System.out.println("Enter the Elements of Array:");
+        
+        for (int i = 0; i <n; i++){
+            numbersArray[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i<n ; i++){
+            sum = sum+numbersArray[i];
+        }
+
+        System.out.println("Sum of its element is; "+ sum);
+
+    }
+    
+}
